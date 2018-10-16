b52cf1cc766c6738a8de1a7986a96fbae11bb343
kimmy truong <kimmy.truong59@gmail.com>
2018-10-15 20:55:44 -0700

    kt

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "KRPh04CxbweUMpbABDGPCxcup"
-consumer_secret = "QPeKIt7nV7bTxWUTEz7wDkFDhK87Lu0ZTDI0tjP4NpZwfNVIlq"
-access_token = "1032509969592025088-UDTgFsFFGgus8AH8KyVYLDElgvo5fp"
-access_token_secret = "2nHhzvozl7gQPz0ZaEn8L97O2IN2EBOYbLteS0XWXtTYl"
